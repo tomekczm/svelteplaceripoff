2665622b3f9444aa66681677a42e5ad4d0240940
tomekczpog <tomekcz@interia.com>
2022-07-23 03:15:29 +0200

    added logging in

---
+++ src/lib/loginmodal.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import { browser } from "$app/env";
+    import Cookies from 'js-cookie'
+    import { loggedIn } from "./writeables";
+
+
+    let password: string
+
+    async function handleSubmit() {
+        if(!browser)
+            return
+
+        const response = await fetch("/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                code: password
+            })
+        })
+        const json = await response.json()
+        if(json.authenticated) {
+            Cookies.set('code', password)
+            loggedIn.set(true)
+            
+        }
+    }
+</script>
+
+<div class="Loginmodal">
+    <div class="Loginmodal-content Center">
+        <form on:submit|preventDefault={handleSubmit}>
+            <label>Enter your code:
+                <input
+                    type="password"
+                    bind:value={password}
+                />
+            </label>
+            <input type="submit"/>
+            <p>too lazy to design this part</p>
+        </form>
+    </div>
+</div>
